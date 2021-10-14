f320d44915ec22cf3eb4957a1b6a1accc803d5ac
gandesirijugalkishore <jugalvs@gmail.com>
2021-10-13 20:55:35 -0500

    Update penguins-app.py

--- penguins-app.py
+++ penguins-app.py
@@ -1,17 +1,16 @@
+import yfinance as yf
 import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-from sklearn.ensemble import RandomForestClassifier
-
 st.write("""
-# Penguin Prediction App
+# STATISTICS FOR LIFE GRADES
 
-This app predicts the **Palmer Penguin** species!
+STUDNET GRADE ANALYTICS
 
-Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
 """)
-
+user_input = st.number_input("NUMBER OF ASSIGNMENTS COMPLETED")
+user_input=int(user_input)
 st.sidebar.header('User Input Features')
 
 st.sidebar.markdown("""
@@ -20,61 +19,57 @@ st.sidebar.markdown("""
 
 # Collects user input features into dataframe
 uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+# if uploaded_file is not None:
 if uploaded_file is not None:
     input_df = pd.read_csv(uploaded_file)
-else:
-    def user_input_features():
-        island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
-        sex = st.sidebar.selectbox('Sex',('male','female'))
-        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-        body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
-        data = {'island': island,
-                'bill_length_mm': bill_length_mm,
-                'bill_depth_mm': bill_depth_mm,
-                'flipper_length_mm': flipper_length_mm,
-                'body_mass_g': body_mass_g,
-                'sex': sex}
-        features = pd.DataFrame(data, index=[0])
-        return features
-    input_df = user_input_features()
+    st.write(input_df.head())
+    c=['Username',
+        'ASSIGNMENT # 1 [Total Pts: 100 Score] |1344236',
+       'ASSIGNMENT # 2 [Total Pts: 100 Score] |1344237',
+       'ASSIGNMENT # 3 [Total Pts: 100 Score] |1344238',
+       'Assignment # 4 [Total Pts: 100 Score] |1344239',
+       'Assignment # 5 [Total Pts: 100 Score] |1344240',
+       'ASSIGNMENT # 6 [Total Pts: 100 Score] |1344241',
+       'ASSIGNMENT # 7 [Total Pts: 100 Score] |1344242',
+       'ASSIGNMENT # 8 [Total Pts: 100 Score] |1344243',
+       'ASSIGNMENT # 9 [Total Pts: 100 Score] |1344244',
+       'ASSIGNMENT # 10 [Total Pts: 100 Score] |1344245']
 
-# Combines user input features with entire penguins dataset
-# This will be useful for the encoding phase
-penguins_raw = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-penguins = penguins_raw.drop(columns=['species'], axis=1)
-df = pd.concat([input_df,penguins],axis=0)
-
-# Encoding of ordinal features
-# https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
-encode = ['sex','island']
-for col in encode:
-    dummy = pd.get_dummies(df[col], prefix=col)
-    df = pd.concat([df,dummy], axis=1)
-    del df[col]
-df = df[:1] # Selects only the first row (the user input data)
-
-# Displays the user input features
-st.subheader('User Input features')
-
-if uploaded_file is not None:
+    st.write(input_df[c].isna().sum().rename("No. OF STUDNETS WHO DID NOT SUBMIT THE ASSIGNEMNT"))
+    grouped_df=input_df.groupby('Username')
+    grp=grouped_df.first()
+    st.write(grp.isnull().sum(axis=1).rename("Number of assignments not submitted"))
+    st.write(input_df["ASSIGNMENT # 1 [Total Pts: 100 Score] |1344236"].isna().groupby(input_df['Username']).sum())
+    df = input_df[c[0:user_input]].copy()
     st.write(df)
+    st.write(input_df[input_df[c[user_input]].isna() == 1]["Username"].rename("STUDNETS NAMES"))
+    
+                                                 
 else:
-    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
-    st.write(df)
+    st.write("""
+# Penguin Prediction App
 
-# Reads in saved classification model
-load_clf = pickle.load(open('penguins_clf.pkl', 'rb'))
+This app predicts the **Palmer Penguin** species!
 
-# Apply model to make predictions
-prediction = load_clf.predict(df)
-prediction_proba = load_clf.predict_proba(df)
+Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
+""")
+ 
 
 
-st.subheader('Prediction')
-penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
-st.write(penguins_species[prediction])
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+#define the ticker symbol
+# tickerSymbol = 'GOOGL'
+#get data on this ticker
+# tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+# tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
 
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
+# st.write("""
+# ## Closing Price
+# """)
+# st.line_chart(tickerDf.Close)
+# st.write("""
+# ## Volume Price
+# """)
+# st.line_chart(tickerDf.Volume)
